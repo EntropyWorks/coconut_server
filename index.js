4b970ed601414c285a472a68db61024cda78cc45
Alexandru <alexandruionascu.bd@gmail.com>
2015-07-22 21:56:49 +0300

    Finally fixed the security

--- index.js
+++ index.js
@@ -19,35 +19,35 @@ app.get('/key', function(req, res) {
 });
 
 io.on('connection', function(socket){
+
+  //handshake
   socket.on('join room', function(data) {
     socket.join(data.sessionId);
     socket['sessionId'] = data.sessionId;
     socket['publicKey'] = data.publicKey;
+
+    console.log(socket.id + " has joined the room " + socket['sessionId']);
   });
 
   socket.on('message', function(data) {
 
-    var messageDecrypted = key.decrypt(data.message, 'utf8');
-
-
-    var clients = io.sockets.adapter.rooms[data.sessionId];
+    var decrypted = key.decrypt(data, 'utf8');
+    var clients = io.sockets.adapter.rooms[socket['sessionId']];
      for (var clientId in clients) {
-
       var clientSocket = io.sockets.connected[clientId];
       //import public key
       key.importKey(clientSocket['publicKey'], 'public');
-
-      var object = {
-        event : data.event,
-        message: key.encrypt(messageDecrypted, 'base64')
-      };
+      //encrypt the data with the clien's public key
+      var encrypted = key.encrypt(decrypted, 'base64');
       //import back the server's key
       key.importKey(serverKey, 'public');
       //check if it is not the same socket
       if(clientSocket.id != socket.id)
-        clientSocket.emit('message', object);
+        clientSocket.emit('message', encrypted);
 
     }
+
+
 
   });
 
